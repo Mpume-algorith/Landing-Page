2efa67cb2cf6aed3d9fb4d3fdeb40341d47c5c70
The A-Team <The A-Team@DESKTOP-B3VVE81>
2022-04-03 01:49:03 +0200

    Implemented Hamburger navigation slide out feature (JavaScript)

---
+++ Landing Page/Scripts/Index.js
@@ -0,0 +1,19 @@
+﻿
+
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link')
+const clickClose = document.querySelector('section')
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
+})
+
+clickClose.addEventListener('click', () => {
+    document.body.classList.remove('nav-open');
+})
