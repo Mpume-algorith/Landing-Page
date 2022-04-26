f9516a3edffd403cb53b01a4875d844dbed0f86a
The A-Team <The A-Team@DESKTOP-B3VVE81>
2022-04-26 14:28:16 +0200

    Modal popup and exit (button only)

--- Landing Page/Scripts/Index.js
+++ Landing Page/Scripts/Index.js
@@ -2,6 +2,18 @@
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link')
 const clickClose = document.querySelector('section')
+const modalOpen = document.querySelector('.about__button');
+const modalClose = document.querySelector('.exit-button');
+var modalContainer = document.querySelector('.modal-container');
+
+modalOpen.addEventListener('click', () => {
+    document.body.classList.add('show');
+})
+
+modalClose.addEventListener('click', () => {
+    document.body.classList.remove('show');
+})
+
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
