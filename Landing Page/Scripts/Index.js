3e122abbe79406d90e275a6d49f5e10de105f14e
The A-Team <The A-Team@DESKTOP-B3VVE81>
2022-05-17 03:10:16 +0200

    Implemented Contact form with Working Email

--- Landing Page/Scripts/Index.js
+++ Landing Page/Scripts/Index.js
@@ -3,17 +3,35 @@ const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link')
 const clickClose = document.querySelector('section')
 const modalOpen = document.querySelector('.about__button');
-const modalClose = document.querySelector('.exit-button');
-var modalContainer = document.querySelector('.modal-container');
+const openSpace = document.querySelector('.modal-container');
+const closeButton = document.querySelector('.exit-button');
+const linkClose = document.querySelector('.project-link');
+const formOpen = document.querySelector('.form-popup');
+const formOpenSpace = document.querySelector('.form-container');
+const formExit = document.querySelector('.form-exit-button');
+
+
+
+function alertMsg() {
+    alert("Successful!");
+}
+
+formOpen.addEventListener('click', () => {
+    document.body.classList.add('show-form');
+})
 
 modalOpen.addEventListener('click', () => {
     document.body.classList.add('show');
 })
 
-modalClose.addEventListener('click', () => {
-    document.body.classList.remove('show');
-})
-
+window.onclick = function (event) {
+    if (event.target == openSpace || event.target == closeButton || event.target == linkClose) {
+        document.body.classList.remove('show');
+    }
+    if (event.target == formOpenSpace || event.target == formExit) {
+        document.body.classList.remove('show-form');
+    }
+}
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
